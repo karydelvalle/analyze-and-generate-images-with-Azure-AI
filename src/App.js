bdc926d94ce9537fb50a391b794f5048ef1c8724
Kary Salas <karydelvalle@gmail.com>
2023-11-25 12:45:46 -0400

    analyze file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React from 'react';
-import {analyzeImage} from './azure-image-analysis.js';
-import './App.css';
+
+// src/App.js
+import React, { useState } from 'react';
+import analyzeImage from './azure-image-analysis';
 
 function App() {
+  const [imageUrl, setImageUrl] = useState('');
+  const [result, setResult] = useState(null);
 
-  function handledAnalizarButton () {
-    analyzeImage()
-    };
+  const handleAnalyze = async () => {
+    try {
+      const analysisResult = await analyzeImage(imageUrl);
+      setResult(analysisResult);
+    } catch (error) {
+      setResult(null);
+    }
+  };
 
-  return <div>
-    
-    
-    <h1>Computer Vision</h1>
-    <input className='' placeholder='URL'></input>
-    <button
-    onClick={() => handledAnalizarButton()}
-    
-    >Submit</button>
-    
-    </div>;
+  return (
+    <div>
+      <h1>React Azure Image Analysis</h1>
+      <input
+        type="text"
+        placeholder="URL de la imagen"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+      <button onClick={handleAnalyze}>Analizar</button>
+
+      {result && (
+        <div>
+          <h2>Resultados:</h2>
+          <pre>{JSON.stringify(result, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
 }
-
-
- 
-      
 
 export default App;
