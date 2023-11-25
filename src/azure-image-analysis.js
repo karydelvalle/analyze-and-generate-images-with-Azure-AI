37ffa34319496f7eba88a3cbeab87ba1e50d489f
Kary Salas <karydelvalle@gmail.com>
2023-11-25 18:37:00 -0400

    url

--- src/azure-image-analysis.js
+++ src/azure-image-analysis.js
@@ -5,8 +5,9 @@ const analyzeImage = async (imageUrl) => {
   try {
     const apiUrl = process.env.REACT_APP_AZURE_API_ENDPOINT;
     console.log('API URL:', apiUrl)
+    
     const response = await axios.post(
-      process.env.REACT_APP_AZURE_API_ENDPOINT,
+      apiUrl,
       {
         url: imageUrl,
       },
