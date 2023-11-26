d239f9bbf5f63ea11c9b7817f8e7017819be4ecd
Kary Salas <karydelvalle@gmail.com>
2023-11-25 23:05:15 -0400

    codigo

--- src/azure-image-analysis.js
+++ src/azure-image-analysis.js
@@ -4,40 +4,56 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const ImageAnalyzer = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState('');
+  const [analysisResult, setAnalysisResult] = useState(null);
 
-  const handleImageChange = (event) => {
-    const file = event.target.files[0];
-    setSelectedImage(file);
+  const handleUrlChange = (event) => {
+    setImageUrl(event.target.value);
   };
 
   const handleAnalysis = async () => {
-    if (selectedImage) {
+    if (imageUrl) {
       try {
         const endpoint = 'https://apppracticaazure.cognitiveservices.azure.com/vision/v4.0/analyze';
         const apiKey = '2f3d5c09acb4480b8b4150a46d9a3896';
 
-        const formData = new FormData();
-        formData.append('image', selectedImage);
+        const params = {
+          visualFeatures: 'Categories,Description',
+          details: 'Celebrities,Landmarks',
+          language: 'en',
+        };
 
-        const response = await axios.post(endpoint, formData, {
+        const response = await axios.post(endpoint, { url: imageUrl }, {
+          params,
           headers: {
-            'Content-Type': 'multipart/form-data',
+            'Content-Type': 'application/json',
             'Ocp-Apim-Subscription-Key': apiKey,
           },
         });
 
-        console.log('Analysis Result:', response.data);
+        setAnalysisResult(response.data);
       } catch (error) {
         console.error('Error analyzing image:', error);
+        setAnalysisResult(null);
       }
     }
   };
 
   return (
     <div>
-      <input type="file" onChange={handleImageChange} />
+      <h2>Azure Computer Vision App</h2>
+      <label>
+        Image URL:
+        <input type="text" value={imageUrl} onChange={handleUrlChange} />
+      </label>
       <button onClick={handleAnalysis}>Analyze Image</button>
+
+      {analysisResult && (
+        <div>
+          <h3>Analysis Result:</h3>
+          <pre>{JSON.stringify(analysisResult, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 };
